db6e206f48aee7ba6c49545f0b7a3eb24081b02d
SomeDom <2009ddominik@gmail.com>
2023-04-13 11:48:59 +0200

    Removed comments

--- src/main/java/net/somedom/pullback/Pullback.java
+++ src/main/java/net/somedom/pullback/Pullback.java
@@ -17,19 +17,16 @@ import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Pullback.MOD_ID)
 public class Pullback {
     public static final String MOD_ID = "pullback";
 
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Pullback() {
-        // Register the setup method for modloading
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
